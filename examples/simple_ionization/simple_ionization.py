c1ffe2475ab9eeabe1f11f4c50fd7189f7b57341
git-user <jgerity@tamu.edu>
2016-06-27 21:02:14 +0000

    Working example of electrons ionizing background gas

--- examples/simple_ionization/simple_ionization.py
+++ examples/simple_ionization/simple_ionization.py
@@ -12,6 +12,8 @@ from warp.particles.extpart import ZCrossingParticles
 from warp.data_dumping.openpmd_diag import particle_diag
 from warp.data_dumping.openpmd_diag import field_diag
 import numpy
+import mpi4py
+import datetime
 
 
 # which geometry to use 2d or 3d
@@ -25,8 +27,8 @@ top.pline2     = "Demonstration of simple ionization, e- + H2 -> e- + e- + H2+"
 top.runmaker   = "James Gerity (jgerity@tamu.edu)"
 
 # --- Invoke setup routine for the plotting
-#setup()
-setup(makepsfile=1, cgmlog=1)
+setup()
+# setup(makepsfile=1, cgmlog=1)
 
 # --- Set basic beam parameters
 ibeaminit = 500e-3   # in A, from Fermilab experiment
@@ -42,22 +44,20 @@ w_cyclotron = echarge*Bsol/emass # Larmor frequency
 top.dt = 1e-9  # fast time step for testing
 print("Timestep is %E s" % top.dt)
 
-top.npinject  = 5.  # Approximate number of particles injected each step
+top.npinject  = 50.  # Approximate number of particles injected each step
 sw = ibeaminit * top.dt / echarge / top.npinject
 
 # relativistic correction
-gamma = ekininit / 511.e6
-beta = 1. - 1./gamma**2
+gamma = numpy.sqrt(1 + ekininit / 511.e6)
+beta = numpy.sqrt(1. - 1./(gamma**2))
 print("Relativistic factors are gamma=%.5f, beta=%.5f" % (gamma,beta))
 
 # --- define Species
-ions = Species(type=Hydrogen, charge_state=+1, name='p', weight=sw)
 electrons = Species(type=Electron, name='e-', weight= sw)
-h2plus = Species(type=Dihydrogen, charge_state=+1, name='H2+', weight= sw)
+h2plus = Species(type=Dihydrogen, charge_state=+1, name='H2+', weight=None)
 
 # jslist is list of species, top.finject[n,i] is fraction of species i to inject at source n
 # Inject only electrons
-top.finject[0, ions.jslist[0]]      = 0
 top.finject[0, electrons.jslist[0]] = 1
 top.finject[0, h2plus.jslist[0]]    = 0
 
@@ -77,7 +77,7 @@ derivqty()
 w3d.l4symtry = false  # four-fold symmetry
 w3d.l2symtry = false  # two-fold symmetry
 
-vz=sqrt(2.*abs(ions.charge)*ions.ekin/ions.mass) # ion velocity
+vz = beta*clight
 
 # --- Set boundary conditions
 
@@ -123,7 +123,7 @@ print("Grid size is %i x %i x %i" % (w3d.nx,w3d.ny,w3d.nz))
 
 # --- Specify injection of the particles
 # --- npmax is the number of simulation particles to create.
-top.npmax     = 50000
+# top.npmax     = 50000
 top.inject    = 1      # 0: no injection, 1: constant current, 2: space-charge limited injection (Child-Langmuir)
 
 top.linj_efromgrid = true  # Turn on transverse E-fields near emitting surface
@@ -135,7 +135,6 @@ w3d.cylinder = true
 # from w3d.F: --- use random numbers to load particles in polar coordinates
 w3d.ldprfile = "polar"
 
-top.inject = 1
 source_radius = 5.5e-3 # 5.5 mm from MAD
 
 top.ainject = source_radius
@@ -143,7 +142,7 @@ top.binject = source_radius
 w3d.l_inj_user_particles_v = true
 
 def nonlinearsource():
-    if w3d.inj_js == ions.jslist[0]:
+    if w3d.inj_js == electrons.jslist[0]:
         np = top.npinject
         r = source_radius*random.random(np)
         theta = 2.*pi*random.random(np)
@@ -183,7 +182,9 @@ f3d.uppasses   =  2
 # --- Generate the PIC code (allocate storage, load ptcls, t=0 plots, etc.)
 package("w3d")
 generate()
-h5output = 'diags/hdf5/'
+
+now = datetime.datetime.now()
+h5output = 'diags_' + now.strftime("%s")
 
 def cleanupPrevious(outputDirectory = 'diags/hdf5/'):
     if os.path.exists(outputDirectory):
@@ -192,10 +193,11 @@ def cleanupPrevious(outputDirectory = 'diags/hdf5/'):
             if file.endswith('.h5'):
                 os.remove(os.path.join(outputDirectory,file))
 
-cleanupPrevious()
+cleanupPrevious(h5output)
 
 #Version of Warp currently installed on Container doesn't handle species weights correctly - 4/21/16
 diagP = particle_diag.ParticleDiagnostic( period=5, top=top, w3d=w3d,
+        write_dir=h5output,
         species= { species.name : species for species in listofallspecies },
         particle_data=["position","momentum"],
         comm_world=comm_world, lparallel_output=True, )
@@ -214,22 +216,25 @@ installconductors(electrode_left + electrode_right)
 # --- Recalculate the fields
 fieldsolve(-1)
 
-target_density = 3.54e19 #1.e-3 torr
+# target_density = 3.54e19 #1.e-3 torr
+target_density = 1.e20 #1.e-3 torr
 
 # --- setup the charge exchange
 # From ionization.py:
 # stride=100: The stride in the particle loops, controlling how many particles
 #               can under go a collision each time step.
 
-ioniz = Ionization(stride=top.npmax / 1000, zmin=0., zmax=le)
+ioniz = Ionization(stride=100, zmin=0., zmax=le)
 
 # ------------ e + H2 -> e + e + H2+
 ioniz.add(incident_species=electrons,
-          emitted_species=[h2plus, electrons],
-          emitted_energy0=[0.026, 0.026], # Array of emission energies corresponding to emitted_species
+          emitted_species=[h2plus],
+          emitted_energy0=0, # Array of emission energies corresponding to emitted_species
           emitted_energy_sigma=0,
+          # Q: What is the distribution of angles of the emitted species?
           l_remove_target=False, # Flag for removing target particle
-          cross_section=1.5e-21, # NIST total ionization cross-section data suggests this is probably ~ 0.01
+          cross_section=1.5e-21, # Where does this figure come from?
+        #   l_verbose=True,
           ndens=target_density)
 
 ixcenter = int(w3d.nx/2)
@@ -247,33 +252,35 @@ for k in range(0, w3d.nz+1):
 eden_time = []
 col_time = []
 
-def doRun():
-    num_steps = 500
-    run_time = 1.e-6
-    final_iter = run_time / top.dt
-    iter = 0
-    while(iter < final_iter):
-        ions.ppzx(color=red, titles = 0, view=9, pplimits=(w3d.zmmin, w3d.zmmax, -top.prwall, top.prwall))
-        pfzx(fill=1,filled=0, plotsg=0,titles = 0, cond=0,view=9)
+plot_repetition = 100
+num_steps = 500
+run_time = 1.e-6
+final_iter = run_time / top.dt
+iter = 0
+diagP.write()
+while(iter < final_iter):
+    if iter % plot_repetition == 0:
+        # ions.ppzx(color=red, titles = 0, view=9, pplimits=(w3d.zmmin, w3d.zmmax, -top.prwall, top.prwall))
+        # pfzx(fill=1,filled=0, plotsg=0,titles = 0, cond=0,view=9)
 
         h2plus.ppzx(color=blue, titles = 0,msize=100, view=10)
         electrons.ppzx(color=green, titles = 0, msize=100, view=10, pplimits=(w3d.zmmin, w3d.zmmax, -top.prwall, top.prwall))
         fma()
 
-        iden  = ions.get_density()
+        # iden  = ions.get_density()
         eden  = electrons.get_density()
         hden  = h2plus.get_density()
 
         ptitles(titlet = "Density (#/m3) at x=y=0", titleb = "Z (m)", titlel =" ")
         limits(w3d.zmmin, w3d.zmmax)
-        pla(iden[ixcenter,iycenter,0:], zcoord, color = red)
+        # pla(iden[ixcenter,iycenter,0:], zcoord, color = red)
         pla(eden[ixcenter,iycenter,0:], zcoord, color = green)
         pla(hden[ixcenter,iycenter,0:], zcoord, color = blue)
         fma()
 
         ptitles(titlet = "Density (#/m3) at ecolumn center", titleb = "X (m)", titlel =" ")
         limits(w3d.xmmin, w3d.xmmax)
-        pla(iden[0:,iycenter,izcenter], xcoord, color = red)
+        # pla(iden[0:,iycenter,izcenter], xcoord, color = red)
         pla(eden[0:,iycenter,izcenter], xcoord, color = green)
         pla(hden[0:,iycenter,izcenter], xcoord, color = blue)
         fma()
@@ -295,10 +302,12 @@ def doRun():
         eden_time.append( (eden[ixcenter,iycenter,izcenter] + eden[ixcenter,iycenter,izcenter-1] + eden[ixcenter,iycenter,izcenter+1])/3. )
         col_time.append( top.time )
 
-        step(num_steps)
-        iter = iter + num_steps
+    hppnum()
 
-    #time history of electron desntiy
-    ptitles(titlet = "Electron density (#/m3) at center", titleb = "Time (s)", titlel =" ")
-    pla(eden_time, col_time,  color= red)
-    fma()
+    step(1)
+    iter = iter + 1
+
+#time history of electron desntiy
+ptitles(titlet = "Electron density (#/m3) at center", titleb = "Time (s)", titlel =" ")
+pla(eden_time, col_time,  color= red)
+fma()
