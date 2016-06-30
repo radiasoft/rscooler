e8c30d8cdd84a283c1fef31fd704e948fdedc3e2
James Gerity <jgerity@tamu.edu>
2016-06-30 23:08:45 +0000

    Updated example to use Chris's new KV distribution and new derived Ionization class

--- examples/simple_ionization/simple_ionization.py
+++ examples/simple_ionization/simple_ionization.py
@@ -1,36 +1,46 @@
 """
-    A simple example for defining and loading a user-defined beam distribution and continuously injecting it.
+    A example demonstrating ionization of background H2 gas by an electron beam
 """
 from __future__ import division
 from warp_init_tools import *   # Not really used but brings in ParticleDiagnostic.
-from warp.particles.ionization import Ionization
+# from warp.particles.ionization import Ionization
+from rswarp.utilities.ionization import Ionization
 from rswarp.diagnostics import FieldDiagnostic
 from rswarp.utilities.beam_distributions import createKV
 import numpy as np
-import os
+import shutil
+from shutil import os
 import random
 from scipy.optimize import newton
 from sys import exit
 
-
-
 diagDir = 'diags/xySlice/hdf5'
+solvertype = 'magnetostatic'
+# solvertype = 'electrostatic'
+fieldperiod = 100  # number of steps between outputting fields
+outputFields = True
+# outputFields = False
 
-def cleanupPrevious(outputDirectory = diagDir):
+
+def cleanupPrevious(outputDirectory=diagDir):
     if os.path.exists(outputDirectory):
         files = os.listdir(outputDirectory)
         for file in files:
-            if file.endswith('.h5'):
-                os.remove(os.path.join(outputDirectory, file))
+            if os.path.isdir(outputDirectory + file):
+                shutil.rmtree(outputDirectory + file, ignore_errors=True)
+            else:
+                os.remove(outputDirectory + file)
+                # if file.endswith('.h5'):
+                #     os.remove(os.path.join(outputDirectory, file))
 
-cleanupPrevious()
+cleanupPrevious('diags/')
 
 #########################
 ### Initialize Plots  ###
 #########################
-# def setup():
-# 	pass
-setup(cgmlog=0)
+
+def setup():
+    pass
 
 ##########################################
 ### Create Beam and Set its Parameters ###
@@ -73,47 +83,57 @@ Lz = (w3d.zmmax - w3d.zmmin)
 dz =  Lz / w3d.nz
 top.dt = (dz) / (beam_beta * clight) / 3  # 3 timesteps to cross a single cell
 ptcl_per_step = beam.ibeam * top.dt // echarge  # number of particles to inject on each step
-print("Timestep is %.2E s" % top.dt)
 
-top.ibpush = 2            # set type of pusher to  vXB push without tan corrections
-                          # 0:off, 1:fast, 2:accurate
+top.ibpush = 2  # 0:off, 1:fast, 2:accurate
 
 # --- Other injection variables
 w3d.l_inj_exact = True
 w3d.l_inj_area = False
 
-w3d.solvergeom = w3d.XYZgeom
+# w3d.solvergeom = w3d.XYZgeom
+w3d.solvergeom = w3d.RZgeom
 
-w3d.bound0 = dirichlet
-w3d.boundnz = dirichlet
+w3d.bound0 = periodic
+w3d.boundnz = periodic
 w3d.boundxy = neumann
 
-solver = MagnetostaticMG()
-# diagF = FieldDiagnostic.MagnetostaticFields(solver, top)
-# installafterstep( diagF.write )
-# solver = MultiGrid3D()
-# diagF = FieldDiagnostic.ElectrostaticFields(solver, top)
-# installafterstep( diagF.write )
-solver.mgtol = [0.01]*3
-registersolver(solver)
+if solvertype == 'electrostatic':
+    solver = MultiGrid3D()
+    diagF = FieldDiagnostic.ElectrostaticFields(solver=solver, top=top, period=fieldperiod)
+elif solvertype == 'magnetostatic':
+    solver = MagnetostaticMG()
+    diagF = FieldDiagnostic.MagnetostaticFields(solver=solver, top=top, period=fieldperiod)
+if solver is not None:
+    solver.mgtol = [0.01] * 3
+    registersolver(solver)
+
+    if outputFields is True:
+        installafterstep(diagF.write)
 
 
+def generateDist():
+    ptclTrans = createKV(
+        npart=ptcl_per_step,
+        a=0.010,
+        b=0.010,
+        emitx=4. * 1.e-6,
+        emity=4. * 1.e-6
+    )
 
-ptclTrans = createKV(
-    npart=ptcl_per_step,
-    a=0.010,
-    b=0.010,
-    emitx=4. * 1.e-6,
-    emity=4. * 1.e-6
-)
+    dv_over_v = 0
+    if dv_over_v > 0:
+        vzoffset = np.random.normal(scale=dv_over_v, size=ptcl_per_step)
+    else:
+        vzoffset = np.reshape([0.0] * ptcl_per_step, (ptcl_per_step, 1))
 
-vz = beam_beta * clight
-zemit = dz / 5  # emitting surface 1/5th of a cell forward
-ptclArray = np.column_stack((ptclTrans, [zemit] * ptcl_per_step, [vz] * ptcl_per_step))
+    vz = beam_beta * clight * ([1.0] * ptcl_per_step + vzoffset)
+    zemit = dz / 5  # emitting surface 1/5th of a cell forward
+    return np.column_stack((ptclTrans, [zemit] * ptcl_per_step, vz))
+
 
 def injectelectrons():
-
-    #Change to x and y angles to velocities
+    ptclArray = generateDist()
+    # Change to x and y angles to velocities
     beam.addparticles(
         x=ptclArray[:, 0],
         y=ptclArray[:, 2],
@@ -123,29 +143,24 @@ def injectelectrons():
         vz=ptclArray[:, 5]
     )
 
-
-
-#Install injector
+# Install injector
 installuserinjection(injectelectrons)
-
-
 
 ####################################
 ### Ionization of background gas ###
 ####################################
 
-
-ioniz = Ionization(stride=100,
-    xmin=w3d.xmmin,
-    xmax=w3d.xmmax,
-    ymin=w3d.ymmin,
-    ymax=w3d.ymmax,
-    zmin=w3d.zmmin,
-    zmax=w3d.zmmax,
-    nx=w3d.nx,
-    ny=w3d.ny,
-    nz=w3d.nz,
-    l_verbose=True)
+ioniz = Ionization( stride=100,
+                    xmin=w3d.xmmin,
+                    xmax=w3d.xmmax,
+                    ymin=w3d.ymmin,
+                    ymax=w3d.ymmax,
+                    zmin=w3d.zmmin,
+                    zmax=w3d.zmmax,
+                    nx=w3d.nx,
+                    ny=w3d.ny,
+                    nz=w3d.nz,
+                    l_verbose=True)
 
 target_density = 1.e20
 
@@ -156,14 +171,13 @@ def crosssection(vi=None):
 
 ioniz.add(incident_species=beam,
           emitted_species=[h2plus, emittedelec],
-          emitted_energy0=[1, 1], # Array of emission energies (in eV) corresponding to emitted_species
-          emitted_energy_sigma=[0, 0.1],
+          emitted_energy0=[lambda vi: 1, lambda vi: 1], # Array of emission energies (in eV) corresponding to emitted_species
+          emitted_energy_sigma=[lambda vi: 0, lambda vi: 0.1],
           l_remove_target=False, # Flag for removing target particle
           # Can this be a function of incidence parameters like energy?
           cross_section=crosssection,
         #   l_verbose=True,
           ndens=target_density)
-
 
 derivqty() #Sets addition derived parameters (such as beam.vbeam)
 
@@ -171,18 +185,11 @@ derivqty() #Sets addition derived parameters (such as beam.vbeam)
 ### Injection Controls ###
 ##########################
 
-
 # --- Specify injection of the particles
 top.inject = 6                       # 2 means space-charge limited injection, 6 is user specified
 top.npinject = ptcl_per_step           # Approximate number of particles injected each step
 top.ainject = 0.0008                      # Must be set even for user defined injection, doesn't seem to do anything
 top.binject = 0.0008                      # Must be set even for user defined injection, doesn't seem to do anything
-
-
-
-
-
-
 
 ############################
 ### Particle Diagnostics ###
@@ -193,18 +200,12 @@ diagP = ParticleDiagnostic( period=1, top=top, w3d=w3d,
         comm_world=comm_world, lparallel_output=False, write_dir = diagDir[:-4] )
 installafterstep( diagP.write )
 
-
 package("w3d")
 generate()
-
 
 step(1)
 
 diagP.write() # You really don't want 1000 diagnostic files
-diagP.period = 10
+diagP.period = 30
 
-step(49)
-
-# uninstalluserinjection(injectelectrons) # Can uninstall injection to stop
-
-step(250)
+stept(0.5e-6)
